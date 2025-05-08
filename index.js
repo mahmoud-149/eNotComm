ea00e967c4c59ee38738efdce691e5824bb2b05b
AhmedAirf <ahmed.airf.x@gmail.com>
2025-05-08 22:24:08 +0300

    products crud operations

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const app = express()
-//const DB = process.env.DB;
+require("dotenv").config();
+const app = express();
+const { products_routes } = require("./routes/productRoutes");
+const mongoose = require("mongoose");
 
+const DB = process.env.DB;
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("error connecting to database", err.message);
+  });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/',(req, res) => {
-return res.status(200).json({
-    message:"done"
+app.use("/api/products", products_routes);
+
+app.use((req, res) => {
+  res.status(500).json({
+    status: 500,
+    data: { data: null, message: "Route not found" },
+  });
 });
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("server running");
 });
-
-
-//const PORT=3000;
-//app.listen(PORT,()=>{
- //   console.log("server running");    
-//})
-module.exports = app;
+// module.exports = app;
