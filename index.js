a7b1da21bede7c087a4d7f9b74d17f8fd1f9082a
mahmoud-149 <mahmoud14ahmedd@gmail.com>
2025-05-08 19:39:28 +0300

    first edition

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express()
+const DB = process.env.DB;
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/',(req, res) => {
+console.log("hello");
+
+});
+
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log("server running");    
+})
